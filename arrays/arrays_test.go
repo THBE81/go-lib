f7863500349f05f9c1973e3407a5e2c99712245c
Thomas Besnard <thomas.besnard81@gmail.com>
2026-04-20 14:40:38 +0200

    add tests

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -2,9 +2,13 @@ package arrays
 
 import "testing"
 
-func TestMap(t *testing.T) {
+func TestGetFirstOfType(t *testing.T) {
 	t.Run("Get first integer element", func(t *testing.T) {
-		// arr := Array("A string", 69, "Your mom", 420, nil, interface{}(0))
-
+		arr := Array("A string", 69, "Your mom", 420, nil, any(0))
+		result, _ := GetFirstOfType[any, int](arr)
+		expected := 69
+		if result != expected {
+			t.Errorf("TestGetFirstOfType()=%d, want %d", result, expected)
+		}
 	})
 }
